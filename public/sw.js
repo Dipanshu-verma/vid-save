d703bf38cd856772660505a720dabd5048ab617e
dhruvswami <dhruvswaminhr@gmail.com>
2026-05-01 13:18:43 +0530

    ad

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,11 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10945839
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+const CACHE_NAME = 'vidsave-v1';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', () => {});

58701b515c835224d4a59696aca6746512a1b701
dhruvswami <dhruvswaminhr@gmail.com>
2026-05-04 17:46:22 +0530

    ad

--- src/pages/Downloader.tsx
+++ src/pages/Downloader.tsx
@@ -79,9 +79,9 @@ export default function Downloader({ downloadState }: DownloaderProps) {
       {/* Ad only after content */}
       {status === 'idle' && <AdPlaceholder label="Advertisement" />}
 
-      {showInterstitial && (
-        <InterstitialAd onClose={() => setShowInterstitial(false)} />
-      )}
+//       {showInterstitial && (
+//         <InterstitialAd onClose={() => setShowInterstitial(false)} />
+//       )}
     </div>
   );
 }
